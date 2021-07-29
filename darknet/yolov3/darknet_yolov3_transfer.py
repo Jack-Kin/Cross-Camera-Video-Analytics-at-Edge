81d5d04f6436eb10c2be7a227efbc1827d6ca733
Jack Kin <jinzhejian@outlook.com>
2021-07-30 02:23:36 +0800

    update transfer code with pre-compilation

---
+++ darknet/yolov3/darknet_yolov3_transfer.py
@@ -0,0 +1,56 @@
+# import numpy as np
+# import os
+# import urllib.request
+# from matplotlib import gridspec
+# from matplotlib import pyplot as plt
+# from PIL import Image
+# from tensorflow.python.platform import gfile
+from rknn.api import RKNN
+
+
+if __name__ == '__main__':
+
+    MODEL_PATH = './yolov3.cfg'
+    WEIGHT_PATH = './yolov3.weights'
+    RKNN_MODEL_PATH = './yolov3_416x234.rknn'
+    DATASET = './dataset.txt'
+
+    # Create RKNN object
+    rknn = RKNN()
+
+    NEED_BUILD_MODEL = True
+
+    if NEED_BUILD_MODEL:
+        # Load darknet model
+        print('--> Loading model')
+        ret = rknn.load_darknet(model=MODEL_PATH, weight=WEIGHT_PATH)
+        if ret != 0:
+            print('Load darknet model failed!')
+            exit(ret)
+        print('done')
+
+        rknn.config(reorder_channel='0 1 2', mean_values=[[0, 0, 0]], std_values=[[255, 255, 255]])
+
+        # Build model
+        print('--> Building model')
+        ret = rknn.build(do_quantization=True, dataset='./dataset.txt', pre_compile=True)
+        if ret != 0:
+            print('Build model failed.')
+            exit(ret)
+        print('done')
+
+        # Export RKNN model
+        print('--> Export RKNN model')
+        ret = rknn.export_rknn(RKNN_MODEL_PATH)
+        if ret != 0:
+            print('Export RKNN model failed.')
+            exit(ret)
+        print('done')
+    else:
+        # Direct load rknn model
+        print('Loading RKNN model')
+        ret = rknn.load_rknn(RKNN_MODEL_PATH)
+        if ret != 0:
+            print('Load RKNN model failed.')
+            exit(ret)
+        print('done')
